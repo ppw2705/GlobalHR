e60554e5749e6d6c86f2637bffca0d77a6fe8bc4
Asus <glatianalva@gmail.com>
2017-10-02 16:37:03 +0530

    1

---
+++ app/src/main/java/com/geeconsystems/globalhr/UI/punch_in_out.java
@@ -0,0 +1,31 @@
+package com.geeconsystems.globalhr.UI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class punch_in_out extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_punch_in_out);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("PUNCH IN-OUT");
+        toolbar.setNavigationIcon(R.drawable.nav);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.add,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
