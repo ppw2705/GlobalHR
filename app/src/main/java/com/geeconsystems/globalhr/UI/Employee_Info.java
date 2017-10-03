2ba498cb0a5d2751cd83cb56289c5ce71633c9ac
urvi <urvi.rambhia@geeconsystems.com>
2017-10-03 15:16:58 +0530

    new files created

--- app/src/main/java/com/geeconsystems/globalhr/UI/Employee_Info.java
+++ app/src/main/java/com/geeconsystems/globalhr/UI/Employee_Info.java
@@ -2,14 +2,21 @@ package com.geeconsystems.globalhr.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.geeconsystems.globalhr.R;
 
 public class Employee_Info extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee__info);
+
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("My Employee Info");
+        toolbar.setNavigationIcon(R.drawable.nav);
+
     }
 }
