f2a3e4101118684d98c5c7bddee8027d4ffc1991
urvi <urvi.rambhia@geeconsystems.com>
2017-10-03 10:33:40 +0530

    splash img screen done

---
+++ app/src/main/java/com/geeconsystems/globalhr/UI/splashscreen.java
@@ -0,0 +1,33 @@
+package com.geeconsystems.globalhr.UI;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.geeconsystems.globalhr.R;
+
+public class splashscreen extends AppCompatActivity {
+    private final static int splashtimmeout=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        ImageView i=(ImageView)findViewById(R.id.logo);
+
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.bounce);
+        i.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               // startActivity(new Intent(splashscreen.this,loginscreen.class));
+                finish();
+            }
+        },splashtimmeout);
+    }
+}
