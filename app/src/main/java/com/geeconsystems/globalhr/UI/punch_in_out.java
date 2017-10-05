58994e1eeabfc0f582bbacad103b62c3d8a27f8e
Asus <glatianalva@gmail.com>
2017-10-05 10:10:02 +0530

    all changes done

--- app/src/main/java/com/geeconsystems/globalhr/UI/punch_in_out.java
+++ app/src/main/java/com/geeconsystems/globalhr/UI/punch_in_out.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.geeconsystems.globalhr.R;
 
 public class punch_in_out extends AppCompatActivity {
     Toolbar toolbar;
+    TextView punchin,punched,time;
+    Button punchout;
+    View line;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,12 @@ public class punch_in_out extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("PUNCH IN-OUT");
         toolbar.setNavigationIcon(R.drawable.nav);
+        punchin=(TextView)findViewById(R.id.punchin);
+        punched=(TextView)findViewById(R.id.punched);
+        time=(TextView)findViewById(R.id.time);
+        punchout=(Button) findViewById(R.id.btnpunchout);
+        line=(View)findViewById(R.id.view);
+
 
     }
     @Override
@@ -25,5 +37,15 @@ public class punch_in_out extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.add,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void punchin(View v)
+    {
+        punchin.setVisibility(View.INVISIBLE);
+        punched.setVisibility(View.VISIBLE);
+        time.setVisibility(View.VISIBLE);
+        punchout.setVisibility(View.VISIBLE);
+        line.setVisibility(View.VISIBLE);
+
     }
 }
