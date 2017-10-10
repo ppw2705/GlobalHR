0c818e94015eff39442b8680b19fa0d280467882
urvi <urvi.rambhia@geeconsystems.com>
2017-10-10 11:04:32 +0530

    changes done

---
+++ app/src/main/java/com/geeconsystems/globalhr/etimesheetbottomdesign.java
@@ -0,0 +1,13 @@
+package com.geeconsystems.globalhr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class etimesheetbottomdesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etimesheetbottomdesign);
+    }
+}
